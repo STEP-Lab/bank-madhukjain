01a8e75f58597f67b24d49b6a8369a6301d0e7be
Madhuri Kondekar <madhurikondekar87@gmail.com>
2018-03-26 23:20:16 +0530

    Added class for Balance.

---
+++ library/src/main/java/com/thoughtworks/bank/Balance.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.bank;
+
+public class Balance {
+    private final double amount;
+
+    public Balance(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
---
+++ library/src/test/java/com/thoughtworks/bank/BalanceTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.bank;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BalanceTest {
+
+    @Test
+    public void shouldReturnGivenAmount() {
+        Balance balance = new Balance(10000);
+        assertThat(balance.getAmount(),is(10000.00));
+    }
+}
